89fb9636a6fad2838c7b397f45a2f109a982d82f
Evgeniya Kostyrya <zhenya241191@gmail.com>
2022-06-03 14:07:52 +0300

    fix: gitignore

---
+++ src/components/initialCards.js
@@ -0,0 +1,32 @@
+export const initialCards = [                                                               //массив мест
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    altText: 'Фотография гор Архыза.'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    altText: 'Фотография заснеженного берега реки в Челябинской области'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    altText: 'Фотография домов в Иваново.'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    altText: 'Фотография горы на Камчатке.'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    altText: 'Фотография железнодорожного пути.'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    altText: 'Фотография зимнего берега байкала.'
+  }
+  ];
